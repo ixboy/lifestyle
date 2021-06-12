62b517fe2d319f18d1ab07b2f57c7fd36e73270e
ixboy <ismaelixboy@gmail.com>
2021-06-12 06:12:39 +0100

    edit controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
+  def current_user
+    current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
