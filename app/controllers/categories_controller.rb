a649354d79b3a1c6dab34e56fe654cb822322e87
ixboy <ismaelixboy@gmail.com>
2021-06-19 04:28:04 +0100

    edit categories and sessions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,63 +1,42 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[show edit update destroy]
 
   def index
-    @category = Category.all
+    @categories = Category.all
   end
 
   def show
-    @categories = Category.all
+    @category = Category.find(params[:id])
+    @articles = Article.category(params[:id]).order_by_most_recent
+    @first = @articles[0]
+    @second = @articles[1]
+    @third = @articles[2]
+    @fourth = @articles[3]
   end
 
   def new
     @category = Category.new
   end
 
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :priority)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name, :priority)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,13 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path notice: "Logged in successfully as: #{@user.name}"
     else
-      flash[:alert] = 'Invalid Name'
+      flash[:alert] = 'Invalid Name. Please signUp'
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out'
+    redirect_to root_path
   end
 end
