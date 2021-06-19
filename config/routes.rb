260aab648823038fa244aab7f44b808e6afb5d50
ixboy <ismaelixboy@gmail.com>
2021-06-19 04:24:21 +0100

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :users
-  # get '/signup', to: 'users#new'
-  # post '/signup', to: 'users#create'
-  root 'articles#index'
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
+    post 'logout' => :destroy
   end
+  resources :articles do
+    resources :votes, only: %i[new create destroy]
+  end
+  resources :categories
+  resources :articles
+  resources :users
+  root 'articles#index'  
 end
