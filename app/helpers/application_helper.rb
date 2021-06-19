5fef37d85736ed10938e8ffe5df59c35eddead0f
ixboy <ismaelixboy@gmail.com>
2021-06-19 04:28:47 +0100

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def display_msg
+    concat content_tag(:p, alert, class: 'alert') if alert.present?
+    content_tag(:p, notice, class: 'notice') if notice.present?
+  end
+
+  def navbar_link_name
+    link_to current_user.name, class: 'navbar-link logout-btn' if logged_in?
+  end
+
+  def navbar_button_logout
+    return unless logged_in?
+
+    button_to 'Logout', logout_path, { method: :delete, data: { confirm: 'Are you sure?' }, class: ' last-btn' }
+  end
+
+  def navbar_link_sign_up
+    link_to 'Sign Up', new_user_path, class: 'navbar-link logout-btn' unless logged_in?
+  end
+
+  def navbar_link_login
+    link_to 'Login |', login_path, class: 'navbar-link logout-btn' unless logged_in?
+  end
+
+  private
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,24 @@
 module ArticlesHelper
+  def display_errors(article)
+    return unless article.errors.full_messages.any?
+
+    content_tag :p, "Article could not be saved. #{article.errors.full_messages.join('. ')}", class: 'errors'
+  end
+
+  def article_item(article)
+    concat link_to Category.find(article.category_id).name.titleize,
+                   category_path(Category.find(article.category_id)), class: 'category-title'
+    concat image_tag article.image, class: 'category-article-image'
+    link_to article.title, article, class: 'category-article-title'
+  end
+
+  def author_name(article)
+    User.find(article.user_id).name.titleize
+  end
+
+  def category_link(article)
+    link_to Category.find(article.category_id).name.titleize,
+            category_path(Category.find(article.category_id)),
+            class: 'category-link'
+  end
 end
