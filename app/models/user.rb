389d8de30eb6327e3836091d3d4bd8f4b648081c
ixboy <ismaelixboy@gmail.com>
2021-06-19 04:32:53 +0100

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :articles, dependent: :destroy
   has_many :votes
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 30,
+                                                               too_long: '30 characters is the maximum allowed' }
+
+  before_validation :normalize_name, on: :create
 end
