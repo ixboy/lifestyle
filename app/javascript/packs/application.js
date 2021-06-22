6efde62d07560df54d67ef33c032abad0c6f3a15
ixboy <ismaelixboy@gmail.com>
2021-06-22 06:54:37 +0100

    change database

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,15 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+import 'bootstrap'
+import "channels"
 
-import "bootstrap"
-import "../stylesheets/application"
+import * as ActiveStorage from "@rails/activestorage"
 
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-  $('.toast').toast({ autohide: false })
-  $('#toast').toast('show')
-})
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
